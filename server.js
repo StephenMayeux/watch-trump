d03f2ba4b0b9bd8b300f4fda455dcf797cf20e6b
Stephen Mayeux <stephenmayeux@gmail.com>
2017-08-16 17:11:57 -0500

    ready for prime time

--- server.js
+++ server.js
@@ -17,26 +17,33 @@ const t = new Twit({
   access_token_secret: process.env.ACCESS_TOKEN_SECRET
 })
 
-const stream = t.stream('statuses/filter', { follow: ['1360915363', '25073877'] })
+const stream = t.stream('statuses/filter', { follow: ['25073877'] })
 
 stream.on('tweet', tweet => {
 
-  const URL = `https://twitter.com/realDonaldTrump/status/${tweet.id_str}`
-  const picture = screenshot(URL, '1024x768', { delay: 3 })
-  const upload = s3Stream.upload({
-    "Bucket": "watch-trump",
-    "Key": `${tweet.id_str}.png`
-  })
+  const { retweeted, retweeted_status, in_reply_to_status_id, in_reply_to_user_id } = tweet
 
-  upload.on('uploaded', fileMeta => {
-    console.log('image successfully uploaded', fileMeta)
-  })
+  if (retweeted || retweeted_status || in_reply_to_status_id || in_reply_to_user_id || tweet.delete) {
+    return
+  }
+  else {
+    const URL = `https://twitter.com/realDonaldTrump/status/${tweet.id_str}`
+    const picture = screenshot(URL, '1024x768', { delay: 10 })
+    const upload = s3Stream.upload({
+      "Bucket": "watch-trump",
+      "Key": `${tweet.id_str}.png`
+    })
 
-  upload.on('error', error => {
-    console.log('error uploading', error)
-  })
+    upload.on('uploaded', fileMeta => {
+      console.log('image successfully uploaded', fileMeta)
+    })
 
-  picture.pipe(upload)
+    upload.on('error', error => {
+      console.log('error uploading', error)
+    })
+
+    picture.pipe(upload)
+  }
 })
 
 app.get('/status', (req, res) => {
